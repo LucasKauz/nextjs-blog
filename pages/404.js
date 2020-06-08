1aa95b8f595a01661e16e937ab84bb566b1d296c
Lucas Marques <lucaskauz@gmail.com>
2020-06-08 14:23:13 -0300

    feat: 404 custom page

---
+++ pages/404.js
@@ -0,0 +1,13 @@
+export default function Custom404() {
+    return (
+        <div style={{
+            display: 'flex',
+            width: '100vw',
+            height: '100vh',
+            justifyContent: 'center',
+            alignItems: 'center'
+        }}>
+            <h1>404 - Not Found</h1>
+        </div>
+    )
+}
